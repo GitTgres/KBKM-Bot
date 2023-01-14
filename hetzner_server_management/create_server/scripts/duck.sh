1210935bcc06a825cf39f2c4030ca720f2746bd7
Tobias <t.greshake@web.de>
2023-01-14 01:51:41 +0100

    use correct path for duck.log

--- hetzner_server_management/create_server/scripts/duck.sh
+++ hetzner_server_management/create_server/scripts/duck.sh
@@ -1,1 +1,1 @@
-echo url="https://www.duckdns.org/update?domains=$1&token=$2&ip=$3" | curl -k -o /usr/src/KBKM-Bot/hetzner_server_management/create_server/scripts/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=$1&token=$2&ip=$3" | curl -k -o ${GITHUB_WORKSPACE}/hetzner_server_management/create_server/scripts/duck.log -K -
