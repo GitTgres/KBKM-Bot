2b0899a4a87945f034f86b4c825e64a9c33aa532
Tobias <t.greshake@web.de>
2023-01-14 02:15:33 +0100

    pass GITHUB_WORKSPACE variable to duck.sh

--- hetzner_server_management/create_server/scripts/duck.sh
+++ hetzner_server_management/create_server/scripts/duck.sh
@@ -1,1 +1,1 @@
-echo url="https://www.duckdns.org/update?domains=$1&token=$2&ip=$3" | curl -k -o $GITHUB_WORKSPACE/hetzner_server_management/create_server/scripts/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=$1&token=$2&ip=$3" | curl -k -o $4/hetzner_server_management/create_server/scripts/duck.log -K -
