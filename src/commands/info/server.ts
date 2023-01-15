1eabe669aa159b7e39b538e5d8776dd81dafffe4
Tobias <t.greshake@web.de>
2023-01-16 00:45:44 +0100

    fix variable name

--- src/commands/info/server.ts
+++ src/commands/info/server.ts
@@ -190,8 +190,8 @@ export default new Command({
                 else if (serverType === 'minecraft')
                 {
                     res.on("close", code => {
-                        console.log(`Minecraft server fertig. Erreichbar unter: ${process.env.duckdnsDomainMINECRAFT}.duckdns.org:25565`);
-                        interaction.followUp(`Minecraft server fertig. Erreichbar unter: ${process.env.duckdnsDomainMINECRAFT}.duckdns.org:25565`);
+                        console.log(`Minecraft server fertig. Erreichbar unter: ${process.env.duckdns_domain_minecraft}.duckdns.org:25565`);
+                        interaction.followUp(`Minecraft server erstellt. Erreichbar unter: ${process.env.duckdns_domain_minecraft}.duckdns.org:25565`);
                     })
                 }
                         //res.stderr.on('data', (data) => {
